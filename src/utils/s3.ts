d32ffad066e50580f9099dde234554aa7179874b
Ethan_rogue <1da18cs115.cs@drait.edu.in>
2021-09-29 01:14:17 +0545

    changed image upload method from presigned URL to S3.Upload with graphql-upload

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,11 +1,13 @@
 import aws from 'aws-sdk'
 import dotenv from 'dotenv'
+import fs from "fs"
 
 dotenv.config()
 
 const region = process.env.AWS_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const bucketName = process.env.AWS_BUCKET_NAME
+// const bucketName = process.env.AWS_BUCKET_NAME
+const bucketName = "ktmsale"
 const secretAccessKey = process.env.AWS_SECRET_KEY_ID
 
 export const s3 = new aws.S3({
@@ -14,6 +16,35 @@ export const s3 = new aws.S3({
   secretAccessKey,
   signatureVersion: 'v4',
 })
+
+export const UploadImage = async (data: any) => {
+  const { createReadStream, filename, mimetype } = await data;
+  const s3Location = await new Promise(async (resolve, reject) => {
+    if (mimetype == 'image/jpeg' || 'image/jpg' || 'image/png') {
+      const stream = createReadStream(
+        {
+          ContentType: 'image/jpeg',
+        }
+      )
+      const params = {
+        Bucket: bucketName,
+        Body: stream,
+        Key: filename,
+        ContentType: mimetype,
+        ContentDisposition: "inline"
+      }
+      const UploadResult = await s3.upload(
+        params
+      ).promise()
+
+      if (UploadResult.Location) return resolve(UploadResult.Location)
+      else reject(`Unable to upload image reason :- ${UploadResult}`)
+    }
+
+  })
+
+  return s3Location
+}
 
 export const generateUploadUrl = async (imgname: string) => {
   const params = {
@@ -27,3 +58,7 @@ export const generateUploadUrl = async (imgname: string) => {
   const uploadurl = await s3.getSignedUrlPromise('putObject', params)
   return uploadurl
 }
+function reason(reason: any, arg1: string) {
+  throw new Error('Function not implemented.')
+}
+
