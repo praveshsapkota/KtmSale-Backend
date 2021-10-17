f234015a865886f264b417d004c54b96c91b371a
Ethan_rogue <1da18cs115.cs@drait.edu.in>
2021-10-17 09:15:37 +0545

    added cors and protected schema

--- src/server-express.ts
+++ src/server-express.ts
@@ -2,9 +2,15 @@ import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import express from 'express'
 import { graphqlUploadExpress } from 'graphql-upload'
-import { schemaWithoutPermissions } from './schema'
+import { schemaWithoutPermissions , schema } from './schema'
 import { createContext } from './context'
 import { createServer } from 'http'
+import cors from "cors"
+// import { ApolloServerPluginUsageReporting } from "apollo-server-core"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 
 const startApolloServer = async (
     schema: any,
@@ -18,34 +24,65 @@ const startApolloServer = async (
     // Same ApolloServer initialization as before, plus the drain plugin.
     const server = new ApolloServer({
         schema: schema,
+        plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+            // ApolloServerPluginUsageReporting({
+            //     rewriteError(err) {
+            //       // Make sure that a specific pattern is removed from all error messages.
+            //       if (err.message.startsWith('Database Error: ')) {
+            //         err.message = err.message.replace(, "REDACTED");
+            //     }
+            //         err.message = err.message.replace(/x-api-key:[A-Z0-9-]+/, "REDACTED");
+            //         return err;
+            //     }
+            //     }),
+        ],
         context: createContext,
-        // typeDefs,
+        formatError : (err)=>{
+            if (err.message.startsWith('Database Error: ')) {
+                console.log("database Error " , err);
+                return new Error('Internal server error')
+            }
+              // Otherwise return the original error. The error can also
+              // be manipulated in other ways, as long as it's returned.
+            return err;
+        },
         // resolvers,
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+        // typeDefs,
+        // introspection: process.env.NODE_ENV !== 'production'
     })
-
     // More required logic for integrating with Express
     await server.start()
 
     // This middleware should be added before calling `applyMiddleware`.
-    app.use(graphqlUploadExpress())
+    app.use(graphqlUploadExpress({
+        maxFileSize: 10000, // 10 MB
+        maxFiles: 20,
+    }))
+    app.use(cors({
+        origin : ['http://localhost:3000', 'https://studio.apollographql.com' , 'ktm-sale.vercel.app' , 'www.praveshsapkota.com.np'],
+        credentials: true,
+        preflightContinue : true,
+        allowedHeaders: ['Authorization'],
+        exposedHeaders: ['Authorization'],
+    }))
+    app.get("/hellow1", (req, res) => {
+        res.send("hellow bro")
+    })
     server.applyMiddleware({
         app,
-
-        // By default, apollo-server hosts its GraphQL endpoint at the
-        // server root. However, *other* Apollo Server packages host it at
-        // /graphql. Optionally provide this to match apollo-server.
         path: '/',
     })
 
     // Modified server startup
     await new Promise((resolve) =>
-        httpServer.listen({ port: 4000 }, (): void =>
+        httpServer.listen({ port: 4000 }, (): void => {
             console.log(
                 `🚀 Server ready at http://localhost:4000${server.graphqlPath}`,
-            ),
+            )
+        }
         ),
     )
 }
 
-startApolloServer(schemaWithoutPermissions)
+startApolloServer(schema)
